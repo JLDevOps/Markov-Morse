beafcceec6b4a70470178b7291763e802d10155d
Jimmy Le <jldevops@gmail.com>
2017-10-17 18:22:34 -0400

    Updated readme, but testing test_markov_morse.py to see where the problem is for travis-clo

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Markov-Morse
+[![Build Status](https://travis-ci.org/JLDevOps/Markov-Morse.svg?branch=master)](https://travis-ci.org/JLDevOps/Markov-Morse)
+
 A python script that reads from either a list of text or input to generate a sentence then interpret as morse code with sound.
--- Script/tests/test_markov_morse.py
+++ Script/tests/test_markov_morse.py
@@ -5,11 +5,8 @@ from Script.morse import *
 
 class test_markov_morse(unittest.TestCase):
     def test_generate_markov_morse(self):
-        try:
-            generate_markov_morse("sherlock", "txt", 1)
-            assert True, "test_markov_morse Successful"
-        except:
-            assert False, "test_markov_morse Failed"
+        generate_markov_morse("sherlock", "txt", 1)
+        assert True, "test_markov_morse Successful"
 
 
 if __name__ == '__main__':
